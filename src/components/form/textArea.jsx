9749e6a62e71a4004bc272cce2a1f409d33cdf58
ryanNS3 <ryansantosn333@gmail.com>
2025-05-18 23:05:31 -0300

    fix/adicionando status de erro no textarea

--- src/components/form/textArea.jsx
+++ src/components/form/textArea.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const TextArea = ({label,id,state,f,colunas=10,linhas=13,...props}) => {
+const TextArea = ({label,id,state,f,colunas=10,linhas=6, error, ...props}) => {
   return (
-    <section className='box-text-area'>
+    <section className={error ? "box-text-area-erro" :'box-text-area'}>
     <label htmlFor={id}>{label}</label>
-      <textarea  className="textarea-compo"  id={id} value={state} onChange={f} {...props}  cols={colunas} rows={linhas}></textarea>
-    
+      <textarea  className={error ? "textarea-compo-erro" :"textarea-compo"}  id={id} value={state} onChange={f} {...props}  cols={colunas} rows={linhas}></textarea>
+      {error &&
+        <p>{error}</p>
+      }
     </section>
   )
 }
